cbb51d0ed9d53cd9220711c0d018eec36880ba0b
Konstantin Antonov <antonovk89@gmail.com>
2022-05-02 18:07:14 +0300

    addproject

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Konstantin Antonov on 02.05.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var bithday: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var text: UITextView!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
